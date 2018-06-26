650b2bdbaaaa11ac49f5a7423c9116a06740becc
merkelAlbert <merkel.albert1997@gmail.com>
2018-06-27 00:07:56 +0300

    stubs for controller added

---
+++ Notes/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notes.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return null;
+        }
+    }
+}
